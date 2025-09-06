cb2fefba441a35bce59a571500b432c4cc7660d8
hebertjosedev <hebertjosedev@gmail.com>
2025-09-06 09:24:27 -0400

    agrego cors para peticiones

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 from routers import products
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://tudominio.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(products.router)
 
